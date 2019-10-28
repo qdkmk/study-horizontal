9821f0ea97dbef0d09b4afb58867dafae8b6563b
qdkmk <qdokuhon@gmail.com>
2019-10-28 13:02:55 +0900

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,32 @@
 import './../styles/appStyles.scss';
 import './../styles/scss.scss';
-import $ from 'jquery';
 
-let item;
-window.onload=()=>{
-  item = document.getElementById('horizontal-list');  
+let sX;
+let minX;
+let windowWidth;
+
+window.onload=()=>{  
+  document.body.style.transition = "1000ms cubic-bezier(0.23, 1, 0.32, 1)"
+  sX = window.scrollX;
+  windowWidth = window.innerWidth;
+  minX = -1 * document.body.clientWidth + windowWidth;
+  console.log("document.body.clientWidth" + document.body.clientWidth);
+  console.log("windowWidth" + windowWidth);
+  console.log("minX" + minX);
 }
 window.addEventListener('wheel', function(e) {
-  if(e.deltaX == 0){
     if (e.deltaY > 0) {
-      //item.scrollLeft += 20;
-      $("body").css("transition","1000mx 'cubic-bezier(0.23, 1, 0.32, 1)'")
-      $("body").css("transform","translateX(-20px)")
+      sX -= 20;
+      if(sX < minX){sX= minX;}
+      let scroll = "translateX(" + sX + "px)";
+      document.body.style.transform = scroll;
       
     }
-    else if(e.deltaY < 0) item.scrollLeft -= 20;
-  }
+    else if(e.deltaY < 0){
+      sX += 20;
+      if(0 < sX){sX=0;}
+      let scroll = "translateX(" + sX + "px)";
+      document.body.style.transform = scroll;
+    } 
+  //}
 });
